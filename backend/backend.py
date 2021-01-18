78258752d2c6544a545942954932c65a630ec170
Tim Kim <timshkim@gmail.com>
2021-01-18 12:03:03 -0800

    linking in progress

--- backend/backend.py
+++ backend/backend.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import request
 from flask import jsonify
-from flask import CORS
+from flask_cors import CORS
 import json
 app = Flask(__name__)
 CORS(app)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Table from './Table'
 import Form from './Form'
+import axios from 'axios';
 
 class App extends Component {
     state = {
@@ -15,9 +16,38 @@ class App extends Component {
         })
     }
 
-    handleSubmit = character =>{
-        this.setState({characters: [...this.state.characters, character]})
+    makePostCall(character){
+        return axios.post('http://localhost:5000/users', character)
+            .then(function(response){
+                console.log(response);
+                return (response.status === 200)
+            })
+            .catch(function(error){
+                console.log(error);
+                return false;
+            });
     }
+
+    handleSubmit = character =>{
+        this.makePostCall(character).then( callResult =>{
+            if(callResult === true) {
+                this.setState({ characters: [...this.state.characters, character] });
+             }
+            }
+        )
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:3000/users')
+         .then(res => {
+           const characters = res.data.users_list;
+           this.setState({ characters });
+         })
+         .catch(function (error) {
+           console.log(error);
+         });
+    }
+
     render() {
         const { characters} = this.state
         return (
